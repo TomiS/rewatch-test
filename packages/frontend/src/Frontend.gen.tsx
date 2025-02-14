0a0aca6a33767fa2eaba3eafd852b875e5de0c27
Tomi Saarinen <tomi.saarinen@iki.fi>
2025-02-14 08:58:24 +0200

    Gentype to work

---
+++ packages/frontend/src/Frontend.gen.tsx
@@ -0,0 +1,10 @@
+/* TypeScript file generated from Frontend.res by genType. */
+
+/* eslint-disable */
+/* tslint:disable */
+
+import * as FrontendJS from './Frontend.bs.js';
+
+import type {Types_payload as Shared_Types_payload} from './Shared.gen';
+
+export const frontendFunction: (arg:Shared_Types_payload) => void = FrontendJS.frontendFunction as any;
---
+++ packages/shared/src/Types.gen.tsx
@@ -0,0 +1,6 @@
+/* TypeScript file generated from Types.res by genType. */
+
+/* eslint-disable */
+/* tslint:disable */
+
+export type payload = { readonly content: string; readonly isValid: boolean };
